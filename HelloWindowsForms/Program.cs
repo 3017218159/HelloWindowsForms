424e4dda6f0e461a98bc109cf576cedefbba70df
lichen <1010043642@qq.com>
2019-03-05 20:42:55 +0800

    更改代码以及图片

--- HelloWindowsForms/Program.cs
+++ HelloWindowsForms/Program.cs
@@ -16,7 +16,7 @@ namespace HelloWindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
 
             MessageBox.Show("Hello World!");
         }
